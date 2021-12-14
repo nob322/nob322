86d8364cea27e6a8d6a97a6633225889420c45b3
David <81187353+nob322@users.noreply.github.com>
2021-12-14 13:00:25 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,12 @@
+/*var es variable global*/var num1 = 50;
+/*let es variable local*/let num2 = 20;
+/*cons nunca se puede modificar durante la ejecución del código*/const num3 = 10;
+var num4 = 10.50;
+let num5 = 99;
+let texto1 = "dejate de joder";
+let verdadero = true;
+let falso = false;
+
+
+let suma = num2 + texto1 + num4;
+document.write(suma);
